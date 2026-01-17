f15880cf4b20780a9d2f1402dc3eabeee43747cc
Sasikumar-Test <sasispartan191@gmail.com>
2026-01-17 10:57:21 +0400

    Add files via upload
    Day-1 Assignment

---
+++ DataTypeAssignment.js
@@ -0,0 +1,26 @@
+//String//
+let firstName = "sasikumar"
+console.log(firstName)
+console.log(typeof firstName)
+//String//
+let companyName = "TestCompany"
+console.log(companyName)
+console.log(typeof companyName)
+//Number//
+let mobileNumber = 8939619640
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+//Boolean//
+let isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+//Undefined//
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
+
+
+
+
+
