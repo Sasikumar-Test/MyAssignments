5bcd817681444bb8cfae54690a159f1fd81d443f
Sasikumar-Test <sasispartan191@gmail.com>
2026-01-17 11:30:14 +0400

    Add files via upload
    Day-2 Assignment

---
+++ isOddOrEven.js
@@ -0,0 +1,14 @@
+function isOddOrEven(Input){
+let Result;
+
+    if (Input%2===0)
+    {
+        Result = "Entered Input is Even"
+    }
+    else
+    {
+        Result = "Entered Input is Odd"
+    }
+    console.log(Result)
+}
+isOddOrEven(334)
