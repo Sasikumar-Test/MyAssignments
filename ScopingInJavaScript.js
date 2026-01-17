dd524dbf55834660dda7bf6d9b3f69600daabc61
Sasikumar-Test <sasispartan191@gmail.com>
2026-01-17 12:21:24 +0400

    Add files via upload
    Day-2 Assignment

---
+++ ScopingInJavaScript.js
@@ -0,0 +1,12 @@
+genderType = "female"
+function printGender(){
+    let color = 'brown'
+    if(genderType=='female')
+    {
+        var age = 30
+        let color = 'pink'
+        console.log(color)
+    }
+    console.log(age)
+}
+printGender()
